0a9522bcd7b8e1a8bbecca0e1aaf36cbd1c2f154
TBSjiga <31573154+TBSjiga@users.noreply.github.com>
2018-05-20 17:35:00 +0300

    Update Human.h

--- Human.h
+++ Human.h
@@ -8,14 +8,14 @@ protected:
 	string surname, name, midname;
 	int age;
 public:
-	Human(string surname, string name, string midname, int age);
+	Human(string &surname, string &name, string &midname, int &age);
 	Human();
 	~Human();
 	virtual void print() =0;
-	void setname(string name) { this->name = name; }
-	void setsurname(string surname) { this->surname = surname; }
-	void setmidname(string midname) { this->midname = midname; }
-	void setage(int age) { this->age = age; }
+	void setname(string &name) { this->name = name; }
+	void setsurname(string &surname) { this->surname = surname; }
+	void setmidname(string &midname) { this->midname = midname; }
+	void setage(int &age) { this->age = age; }
 	string getname() { return name; }
 	string getsurname() { return surname; }
 	string getmidname() { return midname; }
