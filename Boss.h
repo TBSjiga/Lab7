703b1e553bab11067765e79c415a757c53298596
TBSjiga <31573154+TBSjiga@users.noreply.github.com>
2018-05-20 17:34:07 +0300

    Update Boss.h

--- Boss.h
+++ Boss.h
@@ -8,7 +8,7 @@ class Boss :
 protected:
 	int number_of_workers;
 public:
-	Boss(string surname, string name, string midname, int age, int number_of_workers);
+	Boss(string &surname, string &name, string &midname, int &age, int &number_of_workers);
 	Boss();
 	~Boss();
 	void print()
@@ -16,6 +16,6 @@ public:
 		cout << "Boss" << endl << "surname:" << this->name << endl << "name:" << this->name << endl << "midname:" << this->midname << endl << "age:" << this->age << endl
 			<< "number of workers:" << this->number_of_workers<<"." << endl << endl;
 	}
-	void setnumber_of_workers(int number_of_workers) { this->number_of_workers = number_of_workers; }
+	void setnumber_of_workers(int &number_of_workers) { this->number_of_workers = number_of_workers; }
 	int getnumber_of_workers() { return number_of_workers; }
 };
